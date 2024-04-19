61a155310088947052b2dd8a0728f5a8912f271a
aliesal12 <ali.abdulllah121@gmail.com>
2024-04-18 21:10:48 -0700

    remove port

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def model(merchant, city, amount):
         return "Lost"   
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=6000)
+    serve(app)
 
 
 #if __name__ == '__main__':
